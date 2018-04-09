b93f237399f93af8caced257dfe1b815492fe437
giacomo-mangugan <giacomomangugan09091998@gmail.com>
2018-04-09 14:15:38 +0200

    Registrazione completata + NO inserimento dell'utente nel DB

--- app/src/main/java/com/example/matteobellinaso/listspesaandroid/MainActivity.java
+++ app/src/main/java/com/example/matteobellinaso/listspesaandroid/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.matteobellinaso.listspesaandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrazione);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.goToReg);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), RegistrazioneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
