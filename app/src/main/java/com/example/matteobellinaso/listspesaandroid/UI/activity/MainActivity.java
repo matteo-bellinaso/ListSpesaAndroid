2202403631d8558aeab7b80376f897cae88c12d2
matteobellinaso <matteo.bellinaso@gmail.com>
2018-04-09 14:11:11 +0200

    tutorial finished

--- app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/MainActivity.java
+++ app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.matteobellinaso.listspesaandroid.UI.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.matteobellinaso.listspesaandroid.R;
 
@@ -10,8 +13,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tutorial_fragment_first);
+        setContentView(R.layout.activity_main);
+
+
+        Button tutorial = (Button) findViewById(R.id.goToTutorial);
+
+        tutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent tutorialIntent = new Intent(getApplicationContext(), TutorialActivity.class);
+                startActivity(tutorialIntent);
+
+            }
+        });
+
+
+
     }
+
 
 
 
--- app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/TutorialActivity.java
+++ app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/TutorialActivity.java
@@ -1,9 +1,15 @@
 package com.example.matteobellinaso.listspesaandroid.UI.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.matteobellinaso.listspesaandroid.R;
+import com.example.matteobellinaso.listspesaandroid.UI.adapter.SimpleFragmentPageAdapter;
 
 /**
  * Created by matteobellinaso on 09/04/18.
@@ -11,9 +17,26 @@ import com.example.matteobellinaso.listspesaandroid.R;
 
 public class TutorialActivity extends AppCompatActivity {
 
+    private int  currentFrag = 0;
+    public static ViewPager viewPagerz;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tutorial);
+
+        Intent intent = getIntent();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPagerz = viewPager;
+
+        final SimpleFragmentPageAdapter simpleFragmentPageAdapter = new SimpleFragmentPageAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(simpleFragmentPageAdapter);
+
     }
+
 }
+
+
+
