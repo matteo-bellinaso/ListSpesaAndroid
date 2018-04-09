2a55019570b08214193d22a07269bf206750308d
matteobellinaso <matteo.bellinaso@gmail.com>
2018-04-09 11:43:40 +0200

    creazione style tutorial

--- app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/MainActivity.java
+++ app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/MainActivity.java
@@ -1,17 +1,19 @@
-package com.example.matteobellinaso.listspesaandroid;
+package com.example.matteobellinaso.listspesaandroid.UI.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.matteobellinaso.listspesaandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tutorial_fragment_first);
     }
 
 
-    //creazione tutorial
+
 
 }
---
+++ app/src/main/java/com/example/matteobellinaso/listspesaandroid/UI/activity/TutorialActivity.java
@@ -0,0 +1,19 @@
+package com.example.matteobellinaso.listspesaandroid.UI.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.matteobellinaso.listspesaandroid.R;
+
+/**
+ * Created by matteobellinaso on 09/04/18.
+ */
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
